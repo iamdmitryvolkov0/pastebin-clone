e6baadb381f7ea839936231b53d11be72eaa2d6f
iamdmitryvolkov0 <iamdmitryvolkov@gmail.com>
2023-01-30 20:25:36 +0700

    Middlewares added, loop replaced with Scheduler

--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,12 @@ use App\Http\Controllers\UserController;
 
 Route::get('/', [PagesController::class, 'all'])->name('all');
 Route::get('/public', [PagesController::class, 'public'])->name('public');
-Route::get('/private', [PagesController::class, 'private'])->name('private');
+Route::get('/private', [PagesController::class, 'private'])->middleware('auth')->name('private');
 Route::get('/paste/{hash}', [PagesController::class, 'get'])->name('pastePage');
 Route::get('/paste/code/{hash}', [PagesController::class, 'getCode'])->name('pasteCodePage');
 Route::get('/create', [PastesFormsController::class, 'create'])->name('create');
-Route::get('/user_pastes', [PagesController::class, 'userPastes'])->name('userPastes');
-Route::get('/profile', [UserController::class, 'profile'])->name('profile');
+Route::get('/user_pastes', [PagesController::class, 'userPastes'])->middleware('auth')->name('userPastes');
+Route::get('/profile', [UserController::class, 'profile'])->middleware('auth')->name('profile');
 
 Route::post('/store', [PagesController::class, 'store'])->name('store');
 Route::post('/delete', [PagesController::class, 'delete'])->name('delete');
@@ -35,7 +35,7 @@ Route::post('/update', [PagesController::class, 'update'])->name('update');
 
 Route::get('/login', [AuthFormsController::class, 'login'])->name('login');
 Route::post('/login_process', [AuthController::class, 'login'])->name('login_process');
-Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::get('/logout', [AuthController::class, 'logout'])->middleware('auth')->name('logout');
 
 Route::get('/register', [AuthFormsController::class, 'register'])->name('register');
 Route::post('/register_process', [AuthController::class, 'register'])->name('register_process');
