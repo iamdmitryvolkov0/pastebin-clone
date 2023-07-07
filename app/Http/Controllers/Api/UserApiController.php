7065fa4fe7e5a9bb223e908c13bc0c549f2fabcd
Dmitry Volkov <iamdmitryvolkov@gmail.com>
2023-07-07 18:13:23 +0700

    feat(api) & fix(respository -> service)

--- app/Http/Controllers/Api/UserApiController.php
+++ app/Http/Controllers/Api/UserApiController.php
@@ -3,30 +3,56 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CreateUserRequest;
 use App\Repositories\Contracts\UserRepositoryContract;
 use App\Services\Contracts\UserServiceContract;
-use App\Services\UserService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class UserApiController extends Controller
 {
-    public function store()
+    /**
+     * Register new user
+     * @param CreateUserRequest $request
+     * @param UserRepositoryContract $userRepository
+     * @return JsonResponse
+     */
+    public function register(CreateUserRequest $request, UserRepositoryContract $userRepository): JsonResponse
     {
-        //TODO: need to add
+        //TODO: не ловит дату
+        return response()->json([
+            'message' => 'User created',
+            'user' => $userRepository->register($request->validated()),
+        ]);
     }
 
+    /**
+     * Get user profile data
+     * @param UserServiceContract $userService
+     * @return JsonResponse
+     */
     public function profile(UserServiceContract $userService): JsonResponse
     {
         return response()->json($userService->profile());
     }
 
+    /**
+     * Login user
+     * @param UserServiceContract $userService
+     * @param Request $request
+     * @return JsonResponse
+     */
     public function login(UserServiceContract $userService, Request $request): JsonResponse
     {
         $credentials = $request->only('email', 'password');
         return $userService->login($credentials);
     }
 
+    /**
+     * Logout user
+     * @param UserServiceContract $userService
+     * @return JsonResponse
+     */
     public function logout(UserServiceContract $userService): JsonResponse
     {
         return $userService->logout();
